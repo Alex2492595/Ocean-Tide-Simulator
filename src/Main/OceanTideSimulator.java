0fc4ad40728599efdfa5f6bc711fe8caac0d4bb8
2492595 <2492595@host.docker.internal>
2025-11-19 12:10:55 -0500

    Added basic UI for main simulation

--- src/Main/OceanTideSimulator.java
+++ src/Main/OceanTideSimulator.java
@@ -4,7 +4,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package oceantidesimulator;
+package Main;
 
 import java.io.IOException;
 import javafx.application.Application;
